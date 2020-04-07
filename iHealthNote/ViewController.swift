f65690ea02ff0635c64a21054e111a298b205b9e
Juan chin ya <a266280@gmail.com>
2020-04-07 15:49:03 +0800

    Initial Commit

---
+++ iHealthNote/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iHealthNote
+//
+//  Created by Juan Chin Ya on 2020/4/7.
+//  Copyright © 2020 Juan Chin Ya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
